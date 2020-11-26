be51201f9b713e79a10237c34d170af4d735d00c
unknown <szymonrucinski@icloud.com>
2020-11-26 02:31:05 +0100

    initial editing works

--- editor/audio_component/get_audio_peaks.py
+++ editor/audio_component/get_audio_peaks.py
@@ -11,15 +11,15 @@ import scipy.signal
 import scipy
 import librosa 
 import numpy as np
-from detect_peaks import detect_peaks
+from audio_component.detect_peaks import detect_peaks
 
-MIN_CUT_LEN = 0.33
+MIN_CUT_LEN = 0.3
 MAX_CUT_LEN = 3
 
-def get_audio_peaks(path):
+def get_audio_peaks(path, fps):
     x, sr = librosa.load(path)
     indexes = detect_peaks(x, mph = 0.8, mpd=sr/2)
-    print(type(indexes))
+    # print(type(indexes))
     cut_list = []
     for counter,index in enumerate(indexes):
         if counter == 0:
@@ -27,14 +27,16 @@ def get_audio_peaks(path):
         else:
             prev_cut_end = cut_list[-1]['cut_end']
             cut_len = (indexes[counter] - indexes[counter-1])/sr
-            print(cut_len)
+            # print(cut_len)
             if cut_len > MIN_CUT_LEN and cut_len < MAX_CUT_LEN:
-                cut_list.append({'cut_start':prev_cut_end+1,'cut_end':index, 'used':False})
-                print(len(cut_list))
+                cut_start = prev_cut_end+1/sr
+                cut_end = index/sr
+                cut_list.append({'cut_start': round(cut_start,2),'cut_end':round(cut_end,2), 'used':False})
+                # print(cut_list[-1])
                 # print('cut duration: {}'.format(abs(prev_cut_end - index)/sr))
     return cut_list
 
 PATH = 'D:\\Programowanie\\AI\\editor\\data\\samples\\inputs\\temple_of_love-sisters_of_mercy.wav'
 
-get_audio_peaks(PATH)
+# get_audio_peaks(PATH)
 
--- editor/basic_cut.py
+++ editor/basic_cut.py
@@ -1,6 +1,7 @@
 from conf import SAMPLE_INPUTS, SAMPLE_OUTPUTS, AUDIO_DEPT
 from moviepy.editor import *
 from PIL import Image
+import cv2
 import librosa
 import numpy as np
 import pandas as pd
@@ -9,38 +10,23 @@ from scipy.io.wavfile import write
 import scipy.signal
 import scipy
 import librosa 
+import time
 import IPython.display as ipd
 from playsound import playsound
 import numpy as np
-from audio_component.detect_peaks import detect_peaks
 import matplotlib.pyplot as plt
+from audio_component.get_audio_peaks import get_audio_peaks
+
+video_path = os.path.join(SAMPLE_INPUTS, 'movie.mp4')
+audio_path = os.path.join(SAMPLE_INPUTS, 'africa-toto.wav')
+song_path = os.path.join(SAMPLE_INPUTS, 'song.mp3')
 
 
-arr = np.array([1,2,5,6,7,8,12,516,3])
-detect_peaks(arr, mph = 1, mpd =1)
-# indexes = detect_peaks(x, mph = 0.9, mpd=sr/2)
-
-audio_dept = os.path.join(AUDIO_DEPT, 'mov')
-source_path = os.path.join(SAMPLE_INPUTS, 'movie.mp4')
 thumbnail_dir = os.path.join(SAMPLE_OUTPUTS, "thumbnails")
 os.makedirs(thumbnail_dir, exist_ok=True)
-
 thumbnail_dir = os.path.join(SAMPLE_OUTPUTS, "thumbnails")
 thumbnail_per_frame_dir = os.path.join(SAMPLE_OUTPUTS, "thumbnails-per-frame")
 thumbnail_per_half_second_dir = os.path.join(SAMPLE_OUTPUTS, "thumbnails-per-half-second")
-
-
-clip = VideoFileClip(source_path)
-
-print(clip.reader.fps)
-print(clip.reader.nframes)
-duration = clip.duration
-max_duration = int(duration) + 1
-
-fps = clip.reader.fps
-nframes = clip.reader.nframes
-seconds = nframes / (fps*1.0)
-
 #getAll frames
 # for i in range(0, max_duration):
 # # for i in range(0, max_duration + 1):
@@ -53,20 +39,60 @@ seconds = nframes / (fps*1.0)
 #     new_img = Image.fromarray(frame)
 #     new_img.save(new_img_filepath)
 
-def saveFramesToImages():
-    for i, frame in enumerate(clip.iter_frames()):
-        # print(i, frame)
-        fphs = int(fps/2)
-        if i % fphs == 0:
-            current_ms = int((i / fps) * 1000)
-            new_img_filepath = os.path.join(SAMPLE_OUTPUTS, f"{current_ms}.jpg")
-            # print(f"frame at {i} seconds saved at {new_img_filepath}")
-            new_img = Image.fromarray(frame)
-            new_img.save(new_img_filepath)
+# def saveFramesToImages(cut_list):
+#     for i, frame in enumerate(clip.iter_frames()):
+#         # print(i, frame)
+#         fphs = int(fps/2)
+#         if i % fphs == 0:
+#             current_ms = int((i / fps) * 1000)
+#             new_img_filepath = os.path.join(SAMPLE_OUTPUTS, f"{current_ms}.jpg")
+#             # print(f"frame at {i} seconds saved at {new_img_filepath}")
+#             new_img = Image.fromarray(frame)
+#             new_img.save(new_img_filepath)
 
 
 def main():
-    detect_peaks('path')
+    loaded_clip = VideoFileClip(video_path)
+    loaded_music = AudioFileClip(audio_path)
+    duration = loaded_clip.duration
+    fps = loaded_clip.reader.fps
+    nframes = loaded_clip.reader.nframes
+    seconds = nframes / (fps*1.0)
+    cut_list = get_audio_peaks(audio_path, fps)
+    new_sequence = []
+    print(type(new_sequence))
+
+    for i,frame in enumerate(loaded_clip.iter_frames()):
+        if i%2 ==0:
+            new_sequence.append(np.ones([1080,1920,3]))
+            # new_sequence.append(frame)
+        else:
+            new_sequence.append(frame)
+
+
+
+        # gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        # print(type(gray_frame))
+        # print(np.shape(gray_frame))
+    
+
+    clips = [ImageClip(frame).set_duration(1/25) for frame in new_sequence]
+    concat_clip = concatenate_videoclips(clips, method="compose")
+    # concat_clip.set_audio(loaded_clip)
+        # ImageClip(concat_clip).set_audio(audio_path)
+    concat_clip.write_videofile("test.mp4",audio=song_path, codec="mpeg4",fps=25)
+
+    # frames = np.asarray(new_sequence, dtype = None, order = None)
+    # print(type(frames), np.shape(frames))
+    # print(frames[1])
+    # time.sleep(5)
+    # work_clip = ImageSequenceClip(frames[2], fps=6)
+    #         # print(frame)
+
+    # work_clip.write_videofile("render.mp4")
+
+
+main()
     #   [{beginning:20.000, end:44.100,used: boolean}]
     #   convert to numpy array of video
     # remove 10s between [movie_movie_movie_movie_movie]
@@ -74,4 +100,3 @@ def main():
     #get
     #[0-20.000] -> [20.0001-44.100]
     #calculate length
-def get_cuts_position(audio):
